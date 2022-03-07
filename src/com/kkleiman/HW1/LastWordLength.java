48d1ba7f9a27838513ed19d29bfca26ad50262c2
kris <feadog@yandex.ru>
2022-03-07 19:16:21 +0300

    Add solution for LastWordLength

--- src/com/kkleiman/HW1/LastWordLength.java
+++ src/com/kkleiman/HW1/LastWordLength.java
@@ -2,4 +2,12 @@ package com.kkleiman.HW1;
 
 public class LastWordLength {
     public static void main(String[] args) {
+        String s = "Hello This Cruel World"; {
+           s.trim().length();
+           s.trim().lastIndexOf("");
+           System.out.println(s.trim().length() - s.trim().lastIndexOf(" ") - 1);
+
+        }
+
+    }
 }
