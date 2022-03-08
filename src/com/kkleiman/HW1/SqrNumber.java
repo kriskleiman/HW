0cf1ae718d412f4fbb82600e4d2ccf3f078a6259
kris <feadog@yandex.ru>
2022-03-08 13:24:41 +0300

    Add solution for SqrNumber

--- src/com/kkleiman/HW1/SqrNumber.java
+++ src/com/kkleiman/HW1/SqrNumber.java
@@ -1,4 +1,14 @@
 package com.kkleiman.HW1;
 
 public class SqrNumber {
+    public static void main(String args[])
+    {
+        int x = 4;
+        int num=(int)Math.sqrt(x);
+
+        System.out.println(num);
+
+    }
 }
+
+
