9e6fbb3f7f5745b3e4c4280fdb26f204a07df175
kris <feadog@yandex.ru>
2022-03-08 15:14:30 +0300

    Add solution for FibonacciNumber

---
+++ src/com/kkleiman/HW1/FibonacciNumber.java
@@ -0,0 +1,13 @@
+package com.kkleiman.HW1;
+
+public class FibonacciNumber {
+    public static int fib(int n) {
+    if ((n == 0) || (n == 1))
+        return n;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+    public static void main(String[] args) {
+        System.out.println(fib(7));
+        }
+}
